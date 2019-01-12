faf9c519afe51e2811d17425057aa3860e5cf2a6
Adam Uhlir <hello@adam-uhlir.me>
2019-01-12 15:33:21 -0800

    Fixing README in PyPi and other twekas

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@
 import setuptools
 
 setuptools.setup(
-    setup_requires=['pbr>=5.0', 'setuptools>=38.6.0', 'wheel>=0.31.0', 'twine>=1.11.0', 'pytest-runner'],
+    setup_requires=['pbr>=5.0', 'setuptools>=38.6.0', 'wheel>=0.31.0', 'twine>=1.11.0'],
     pbr=True
 )
