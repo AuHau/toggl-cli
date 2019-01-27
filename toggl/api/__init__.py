a6931c5d8984737cce31da18187439179f15fb88
Sander van Rijn <svr003@gmail.com>
2019-01-27 20:28:57 +0100

    Adding Tag to api.__init__

--- toggl/api/__init__.py
+++ toggl/api/__init__.py
@@ -1,1 +1,1 @@
-from toggl.api.models import Client, Workspace, Project, User, WorkspaceUser, ProjectUser, TimeEntry, Task
+from toggl.api.models import Client, Workspace, Project, User, WorkspaceUser, ProjectUser, TimeEntry, Task, Tag
