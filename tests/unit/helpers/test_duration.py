333f1b4ed3a230d79dded04bf6a1847eebe80b8e
figarocorso <figarocorso@gmail.com>
2020-11-15 02:17:34 +0100

    Return two digits format at duration_format

--- tests/unit/helpers/test_duration.py
+++ tests/unit/helpers/test_duration.py
@@ -4,11 +4,11 @@ from toggl.cli.helpers import format_duration
 class TestDuration:
 
     def test_duration_format_seconds(self):
-        assert format_duration(9) == '0:0:9'
-        assert format_duration(11) == '0:0:11'
+        assert format_duration(9) == '0:00:09'
+        assert format_duration(11) == '0:00:11'
 
     def test_duration_format_minutes(self):
-        assert format_duration(60 + 11) == '0:1:11'
+        assert format_duration(60 + 11) == '0:01:11'
         assert format_duration(20 * 60 + 11) == '0:20:11'
 
     def test_duration_format_hours(self):
