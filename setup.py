15f9edc09b785a604d0d97380293dddd7383e3a5
Dominik Stańczak-Marikin <stanczakdominik@gmail.com>
2023-02-12 20:47:50 +0100

    ci: swapping travis for github actions, take 2 (#279)
    Co-authored-by: Adam Uhlíř <adam@uhlir.dev>

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@
 import setuptools
 
 setuptools.setup(
-    setup_requires=['pbr>=5.0', 'setuptools>=38.6.0', 'wheel>=0.31.0', 'twine>=1.11.0'],
-    pbr=True
+    setup_requires=['setuptools>=39.2.0', 'wheel>=0.31.0', 'twine>=1.11.0'],
 )
