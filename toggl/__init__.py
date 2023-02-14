4829a629e11d77975a8cf1fe3c3638c1a73c0765
Adam Uhlíř <adam@uhlir.dev>
2023-02-14 19:47:21 +0100

    revert: pbr removal

--- toggl/__init__.py
+++ toggl/__init__.py
@@ -1,7 +1,11 @@
-__version__ = '2.4.2'
+from pbr.version import VersionInfo
+
+VERSION = VersionInfo('togglCli').semantic_version()
+__version__ = VERSION.release_string()
 
 __all__ = (
     '__version__',
+    'VERSION',
     'api',
     'utils',
     'exceptions',
